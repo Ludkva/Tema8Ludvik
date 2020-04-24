402c1defa59421b878f90e825c7cb1c5902dfc72
Ludkva <ludvikkvalsvik@gmail.com>
2020-04-24 09:24:38 +0200

    First commit

--- profile-vanilla/index.js
+++ profile-vanilla/index.js
@@ -2,3 +2,15 @@ let Namn = document.querySelector(`#inpNamn`)
 let Email = document.querySelector(`#inpEmail`)
 let Submit = document.querySelector(`#Confirm`)
 let Form = document.querySelector(`.Form`)
+
+
+Submit.addEventListener('click', okButton)
+
+function okButton(){
+    vertifisering()
+}
+function vertifisering(){
+    
+
+
+}
